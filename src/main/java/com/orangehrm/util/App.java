0bcfda7ee7ff88ee5ea80b1a40fd18b0d84833d1
Amrapalisu <amrapali.suryawanshi97@gmail.com>
2025-04-15 14:14:02 +0530

    pls check this file

---
+++ src/main/java/com/orangehrm/util/App.java
@@ -0,0 +1,9 @@
+package com.orangehrm.util;
+
+public class App {
+	private void getAppUrl() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/orangehrm/util/Locator.java
@@ -0,0 +1,9 @@
+package com.orangehrm.util;
+
+public class Locator {
+	public static String getLocator() {
+return PropertiesUtil.getProperty(key);
+
+	}
+
+}
