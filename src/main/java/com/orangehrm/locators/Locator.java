92a56016f5ed0de499c24bd6099526189f633664
Amrapalisu <amrapali.suryawanshi97@gmail.com>
2025-04-15 17:50:04 +0530

    new line added pls check

--- src/main/java/com/orangehrm/locators/Locator.java
+++ src/main/java/com/orangehrm/locators/Locator.java
@@ -20,6 +20,7 @@ String subtn = "//button[@type=\"submit\"]";
 String JobTitlesh = "//h6[text()='Job Titles']";
 
 String Job = "//h6[text()='Job Titles']";
+String Jobfinish = "//h6[text()='Job Titles']";
 
 
 }
