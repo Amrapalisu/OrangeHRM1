8bb83c3846e92f85f7a8e79669fd4120d1337c91
Amrapalisu <amrapali.suryawanshi97@gmail.com>
2025-05-20 13:24:47 +0530

    adding in testbase class App method qa

--- src/main/java/com/orangehrm/testbase/TestBase.java
+++ src/main/java/com/orangehrm/testbase/TestBase.java
@@ -11,6 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.orangehrm.Keyword;
+import com.orangehrm.util.App;
 
 public class TestBase {
 	
@@ -19,11 +20,11 @@ public class TestBase {
 	public void setUp() throws Exception {
 		Keyword keyword = new Keyword();
 		keyword.launchBrowser("Chrome");
-        keyword.launchurl("https://opensource-demo.orangehrmlive.com");		
+        keyword.launchurl(App.getAppUrl("qa"));		
 	}
 
 	@AfterMethod
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception{
 		// logOut();
 		// Thread.sleep(3000);
 		Keyword.driver.quit();
