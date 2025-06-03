90fd0049a5c2bcddc4dd16eba67a6f7651cbf00b
Madhuri1392 <mokashe.madhuri@gmail.com>
2025-06-03 13:57:17 +0530

    add files

--- src/main/java/com/orangehrm/pages/PimPage.java
+++ src/main/java/com/orangehrm/pages/PimPage.java
@@ -12,35 +12,42 @@ public class PimPage {
 	@FindBy(css = "input[name='firstName']")
 
 	WebElement FristName;
+	
 	@FindBy(css = "input[name=\"middleName\"]")
 	WebElement MiddleName;
+	
 	@FindBy(css = "input[name=\"lastName\"]")
 	WebElement LastName;
+	
 	@FindBy(css = "button[type=\"submit\"]")
 	WebElement SubmitB;
+	
 
 	public PimPage() {
 		PageFactory.initElements(Keyword.driver, this);
 
 	}
-public void clickonAddemployee() {
-Addemployee.click();
-}
 
+	public void clickonAddemployee() {
+		Addemployee.click();
+	}
 
-public void Fristname(String Fristname) {
-FristName.sendKeys(Fristname);
-}
-public void middlename(String Middlename) {
-MiddleName.sendKeys(Middlename);
-}
-public void lastname(String Lastname) {
-LastName.sendKeys(Lastname);
-}
+	public void Fristname(String Fristname) {
+		FristName.sendKeys(Fristname);
+	}
 
-public void waitForAddEmployeeToBevisibile() {
-	Keyword keyword = new Keyword();
-	keyword.waitForElementToBeVisible(Addemployee);
-}
+	public void middlename(String Middlename) {
+		MiddleName.sendKeys(Middlename);
+	}
+
+	public void lastname(String Lastname) {
+		LastName.sendKeys(Lastname);
+	}
 	
+
+	public void waitForAddEmployeeToBevisibile() {
+		Keyword keyword = new Keyword();
+		keyword.waitForElementToBeVisible(Addemployee);
+	}
+
 }
